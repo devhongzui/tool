a301b8f58f4cbf4e671cce08c6fe8de65bd34e9f
Tri Nguyen Duc <trind@ecommage.com>
2023-10-05 19:22:59 +0700

    Clean code

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let router = express.Router();
-
-/* GET home page. */
-router.get("/", (_, res) =>
-  res.status(200).json({ message: `Welcome to ${process.env.NAME}` })
-);
-
-module.exports = router;
