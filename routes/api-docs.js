cfead415e65615a9cabfb2218218b1bfa55d7108
Tri Nguyen Duc <trind@ecommage.com>
2023-10-14 16:43:01 +0700

    Remove Swagger

--- routes/api-docs.js
+++
@@ -1,6 +0,0 @@
-import swaggerJSDoc from "swagger-jsdoc";
-import SwaggerOptions from "../config/swagger.js";
-
-let SwaggerDoc = swaggerJSDoc(SwaggerOptions);
-
-export default SwaggerDoc;
--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import { expressMiddleware } from "@apollo/server/express4";
 import server from "./config/graphql.js";
 import cors from "cors";
 import UrlOptions from "./config/urlEncoded.js";
-import SwaggerDoc from "./routes/api-docs.js";
-import swaggerUiExpress from "swagger-ui-express";
 import NotFound from "./controllers/notFound.js";
 import ErrorHandle from "./controllers/errorHandle.js";
 
@@ -22,41 +20,15 @@ app.use(express.json());
 app.use(urlencoded(UrlOptions));
 app.use(express.static("./public"));
 
-let path = {
-  api: "/api",
-  docs: "/docs",
-};
-
 // routes
-app.use(path.api, express.json(), cors(), expressMiddleware(server));
-app.use(path.docs, swaggerUiExpress.serve, swaggerUiExpress.setup(SwaggerDoc));
+app.use("/api", express.json(), cors(), expressMiddleware(server));
 app.use(NotFound);
 app.use(ErrorHandle);
 
-let url = process.env.APP_URL;
-let port = process.env.APP_PORT;
+let nodejsPath = `http://${process.env.APP_URL}:${process.env.APP_PORT}`;
+let mongodbPath = `mongodb://${process.env.DB_USERNAME}:*****@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?authSource=admin`;
 
-let db_username = process.env.DB_USERNAME;
-let db_password = encodeURIComponent(process.env.DB_PASSWORD);
-let db_host = process.env.DB_HOST;
-let db_port = process.env.DB_PORT;
-let db_database = process.env.DB_DATABASE;
-
-class Row {
-  constructor(service, address) {
-    this.service = service;
-    this.address = address;
-  }
-}
-
-app.listen(port, () => {
-  console.table([
-    new Row("NodeJs", `http://${url}:${port}`),
-    new Row(
-      "MongoDB",
-      `mongodb://${db_username}:${db_password}@${db_host}:${db_port}/${db_database}?authSource=admin`
-    ),
-    new Row("GraphQL", `http://${url}:${port}${path.api}`),
-    new Row("Swageer", `http://${url}:${port}${path.docs}`),
-  ]);
+app.listen(process.env.APP_PORT, () => {
+  console.log(`NodeJs: ${nodejsPath}`);
+  console.log(`MongoDB: ${mongodbPath}`);
 });
